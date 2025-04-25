d62b823cd2c1eae5761acd03e113fc6fa78f7c4e
isaisp <110698701+isaisp@users.noreply.github.com>
2025-04-25 08:26:50 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Random;
+
+class Main {
+    Random RNG = new Random();
+    
+    public String Roll(String Choice) {
+        if(Choice == "Rock")
+    }
+    
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner();
+        System.out.println("Rock paper scissors shoot!");
+        System.out.println("Choose either: Rock, Paper or Scissors");
+        String choice = scnr.next();
+        if(choice == "rock" || choice == "Rock") {
+            
+        }
+    }
+}
